d19088a6130ce04e4aae7c8cd507de91c4bb5915
Yeram Lee <coramdeo643@kakao.com>
2025-04-28 15:49:41 +0900

    250428.1500.swing,image_upload

--- src/_my_test/MyPaintFrame.java
+++ src/_my_test/MyPaintFrame.java
@@ -14,7 +14,7 @@ public class MyPaintFrame extends JFrame {
 
     private void initData() {
         setTitle("MyPaintFrame");
-        setSize(500,500);
+        setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
@@ -29,25 +29,37 @@ public class MyPaintFrame extends JFrame {
         @Override
         public void paint(Graphics g) {
             super.paint(g);
-            g.drawLine(150,50,90,110);
-            g.drawLine(150,50,210,110);
-            g.drawRect(100,100,100,100);
-            g.drawRect(160,120,20,20);
-            g.drawLine(170,120,170,140);
-            g.drawLine(160,130,180,130);
+            g.drawLine(150, 50, 90, 110);
+            g.drawLine(150, 50, 210, 110);
+            g.drawRect(100, 100, 100, 100);
+            g.drawRect(160, 120, 20, 20);
+            g.drawLine(170, 120, 170, 140);
+            g.drawLine(160, 130, 180, 130);
+            int[] xPoints = {300, 350, 400, 290, 410, 300};
+            int[] yPoints = {300, 200, 300, 230, 230, 300};
+            g.drawPolyline(xPoints, yPoints, 6);
 
             for (int i = 0; i < 30; i++) {
-                g.drawLine(i*10,5,150,50);
-                g.drawLine(400,i*5,200,200);
+                g.drawLine(i * 10, 5, 150, 50);
             }
             for (int i = 0; i < 11; i++) {
-                g.drawRect(10,300,i*10,i*10);
+                g.drawRect(10, 250, i * 10, i * 10);
             }
+            for (int i = 0; i < 101; i += 10) {
+                for (int j = 0; j < 101; j += 10) {
+                    g.drawString("*", i + 300, j + 50);
+                }
+            }
+
+            for (int i = 1; i < 102; i += 20) {
+                g.drawArc(200, 200, i, i, 0, 360);
+            }
+
+
         }
     } // end of static inner class
 
     public static void main(String[] args) {
         new MyPaintFrame();
     } // end of main
-
-}
+} // end of class
---
+++ src/_swing/MyImageFrame.java
@@ -0,0 +1,65 @@
+package _swing;
+
+import _my_test.MyPaintFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyImageFrame extends JFrame {
+
+    private MyImagePanel myImagePanel;
+
+
+    public MyImageFrame() {
+        init();
+        setInit();
+    }
+
+    private void init() {
+        setTitle("Upload image practice");
+        setSize(500,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        myImagePanel = new MyImagePanel();
+    }
+
+    private void setInit() {
+        add(myImagePanel);
+    }
+
+    static class MyImagePanel extends JPanel {
+        // member variable declare of inner class
+        private Image image1;
+        private Image image2;
+        private Image image3;
+        private Image image4;
+        // inner class constructor
+        public MyImagePanel() {
+            // ImageIcon 데이터 타입을 .getImage() 메서드 호출해서 형변환 > Image
+            // ImageIcon(파일명) 파일찾는 기준, 루트폴더 java_useful 아래를 확인함
+            image1 = new ImageIcon("image2.png").getImage();
+            image2 = new ImageIcon("image1.png").getImage();
+            image3 = new ImageIcon("i.png").getImage();
+            image4 = new ImageIcon("o.png").getImage();
+        }
+
+        // 자동으로 호출되게끔 순서가 정해져있음
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            // 이미지를 올려보자~
+            g.drawImage(image1, 0, 0,  200,200, null);
+            g.drawImage(image2, 200, 0,  200,200, null);
+            g.drawImage(image3, 200, 200,  200,200, null);
+            g.drawImage(image4, 0, 200,  200,200, null);
+
+        }
+    } // end of static inner class
+
+    public static void main(String[] args) {
+        new MyImageFrame();
+    } // end of main
+
+} // end of class
+
